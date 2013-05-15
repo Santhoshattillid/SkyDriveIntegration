c88539e8517875e0042bc131078a5c1643f7707b
Santhosh <Santhosh@tillidsoft.com>
2013-05-15 12:27:10 +0530

    updates

--- SkyDriveIntegration/DropBoxChooser.aspx.cs
+++ SkyDriveIntegration/DropBoxChooser.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HgCo.WindowsLive.SkyDrive;
 
 namespace SkyDriveIntegration
 {
@@ -6,7 +7,27 @@ namespace SkyDriveIntegration
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //const string folderName = "Folder4";
 
+            var client = new SkyDriveServiceClient();
+
+            client.LogOn("santhosh@tillidsoft.com", "password@123");
+
+            //var info = client.CreateRootWebFolder(folderName, WebFolderCategoryType.Photos,WebFolderItemShareType.Public);
+
+            //foreach (WebFolderInfo subWebFolder in client.ListRootWebFolders())
+            //{
+            //    if (subWebFolder.Name == folderName)
+            //    {
+            //        var fileInfo = client.UploadWebFile(@"C:\Users\Santhosh.TILLID\Desktop\speed_up_software_development.jpg",subWebFolder);
+
+            //        var url = "https://skydrive.live.com/?cid=" + fileInfo.OwnerCid + "&id=" + fileInfo.ResourceId;
+            //    }
+            //}
+
+            //var webDriveInfo =  client.GetWebDriveInfo();
+
+            //var accountInfo =  client.GetWebAccountInfo();
         }
     }
 }
